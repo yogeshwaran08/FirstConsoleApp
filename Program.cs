a1b0b1d580de1c5bb2f4e8fdfcd6e8a5ca82b349
Yogeshwaran R <yogeshwaran08083@gmail.com>
2023-08-22 05:53:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+while (true)
+{
+    char str1;
+
+    Console.Write("Enter the Alphabet : ");
+    try
+    {
+        str1 = Console.ReadLine()[0];
+    }
+    catch
+    {
+        Console.WriteLine("Found empty input exiting");
+        break;
+    }
+
+
+    if ((str1 == 'A' || str1 == 'a') || (str1 == 'E' || str1 == 'e') || (str1 == 'i' || str1 == 'I') ||
+        (str1 == 'o' || str1 == 'O') || (str1 == 'u' || str1 == 'U'))
+    {
+        Console.WriteLine("You have entered a volwel");
+    }
+    else
+    {
+        Console.WriteLine("You have entered a consonent");
+    }
+
+    if (char.ToUpper(str1) == str1)
+    {
+        Console.WriteLine("The given letter to capital");
+    }
+    else
+    {
+        Console.WriteLine("The given letter to small");
+    }
+}
